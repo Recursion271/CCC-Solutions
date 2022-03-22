528008ecac27fa4bf34d0b95a67a299da806fa14
Recursion271 <nadirshaan01@gmail.com>
2022-03-22 18:14:43 -0400

    Solution to J1

--- 2022/JI.py
+++ 2022/JI.py
@@ -1,1 +1,15 @@
-print('Hello World 2')
+regularBox = int(input("Amount of Regular boxes"))
+smallBox = int(input("Amount of small boxes"))
+students = 28
+amountofcupcakes = regularBox * 8 + smallBox *3
+remainingcupcakes = amountofcupcakes - students
+print (remainingcupcakes) 
+
+
+
+
+
+
+
+
+
